bd27adc9169b6bd6d229df499e9cce0584695041
John S <johnc.sime@gmail.com>
2022-01-19 14:16:21 -0500

    created the structure for the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Fact from './componets/Fact';
 
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <div className='header'>
+        <h1>Random Facts Generator</h1>
+      </div>
+      <Fact />
     </div>
   );
 }
---
+++ src/componets/Fact.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+const Fact = ()=> {
+    return (
+        <>
+        <div>
+            <p>Fun fact: </p>
+        </div>
+        <button>New Fact</button>
+        </>
+    )
+};
+
+
+export default Fact;
